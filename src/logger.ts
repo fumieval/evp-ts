fba72a55ba7f8e44b9500839e95c4eb06618457f
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2024-05-08 15:11:59 +0900

    logger: re-format

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,4 @@
-import pc from "picocolors"
+import pc from 'picocolors';
 
 export interface ILogger {
     present(key: string, value: string): void;
@@ -9,15 +9,17 @@ export interface ILogger {
 
 export class ConsoleLogger implements ILogger {
     public present(key: string, value: string): void {
-        console.info(`${pc.blue("[EVP]")} ${key}=${value}`);
+        console.info(`${pc.blue('[EVP]')} ${key}=${value}`);
     }
     public useDefault(key: string, value: string): void {
-        console.info(`${pc.gray("[EVP]")} ${key}=${value} (default)`);
+        console.info(`${pc.gray('[EVP]')} ${key}=${value} (default)`);
     }
     public missing(key: string): void {
-        console.error(`${pc.red("[EVP]")} ${key} is missing`);
+        console.error(`${pc.red('[EVP]')} ${key} is missing`);
     }
     public error(key: string, value: string, error: Error): void {
-        console.error(`${pc.red("[EVP]")} ${key}=${value} ERROR: ${error.message}`);
+        console.error(
+            `${pc.red('[EVP]')} ${key}=${value} ERROR: ${error.message}`,
+        );
     }
 }
